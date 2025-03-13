55ee245dfc886901ff6e68ffae02ac4d81ae2d15
Ziad Assem <ziadassem3000@gmail.com>
2025-03-13 10:34:57 +0200

    Final Product

--- constants.py
+++ constants.py
@@ -1,8 +1,3 @@
-# Database Configuration Constants
-_DB_HOST = "car-show-database.c3eycucmyyxl.us-east-1.rds.amazonaws.com"
-_DB_USER = "admin"
-_DB_PASSWORD = "admin123"
-_DB_NAME = "car_showroom_database"
-_FIRST_BRANCH="smouha"
-
-
+################################################
+#############confidential#######################
+################################################
--- main.py
+++ main.py
@@ -1,34 +1,6 @@
-#####TESTING DB FUNCTIONS#######
-from branches import Branch
 import functions
 import constants
 
-
-
-# smouha_branch=Branch("smouha")
-# gleem_branch=Branch("gleem")
-# #
-# # gleem_branch.add_car("gleem", "Toyota", "Corolla", "Sedan", "White", 25000.00)
-# # # #
-# # smouha_branch.add_car("smouha", "Honda", "Civic", "Sedan", "Black", 27000.50)
-# # # #
-# # gleem_branch.add_car("gleem", "Ford", "Mustang", "Coupe", "Red", 55000.75)
-# # # #
-# gleem_branch.list_all_branch_cars()
-# # sodo_branch.add_car("sodo", "Chevrolet", "Camaro", "Sports", "Yellow", 60000.00)
-# #
-# # sodo_branch.add_car("sodo", "BMW", "X5", "SUV", "Blue", 75000.99)
-#
-# cars, table = sodo_branch.display_cars("sodo")
-# branch="sodo"
-#
-# # print(f"{table} \n with len of: {cars[0][0]}")
-# sodo_branch.purchase_car(branch)
-# # sodo_branch.purchase_car()
-
-#######################################################################
-#######################################################################
-#######################################################################
 branches_objects={}
 
 #initializing with smouha, the first branch
